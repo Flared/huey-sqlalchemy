dd1246da82011a22c1ada66c282de72b29fc8a63
isra17 <isra017@gmail.com>
2019-10-22 16:47:15 -0400

    Add setup.py and README

---
+++ README.rst
@@ -0,0 +1,4 @@
+A `Huey <https://github.com/coleifer/huey>`_ Storage implementation for SQLAlchemy.
+
+An SQL implementation `already exists <https://github.com/coleifer/huey/blob/master/huey/contrib/sql_huey.py>`_
+for Peewee, but this one might be useful if a project is already using SQLAlchemy.
---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup, find_packages
+
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as fh:
+    readme = fh.read()
+
+setup(
+    name='huey-sqlalchemy',
+    version=__import__('huey_sqlalchemy').__version__,
+    description='Huey Storage using SQLAlchemy',
+    long_description=readme,
+    author='Israël Hallé',
+    author_email='israel.halle@flare.systems',
+    url='http://github.com/Flared/huey-sqlalchemy/',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: LGPL License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    test_suite='runtests.runtests',
+)
